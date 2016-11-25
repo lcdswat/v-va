2bdb656c44c37f5dc214a4d2db2dfa2f1d195080
lcdswat <lcd-swat@yandex.ru>
2016-11-25 13:56:51 +0300

    Add files via upload

---
+++ branding/pages.js
@@ -0,0 +1,8 @@
+var pages = [
+	'branding/fitness_guides.shtml',
+	'branding/chertovsky.shtml',
+	'branding/lic79.shtml',
+	'branding/labrem.shtml',
+	'branding/macaronsLab.shtml',
+	'branding/nezhnyeFeshi.shtml',
+];
